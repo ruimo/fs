c62a51bf122cf91f80af3005e8309f416ed925ad
Shisei Hanai <ruimo.uno@gmail.com>
2019-10-07 17:18:42 +0900

    refs #9

--- app/models/PagedRecords.scala
+++ app/models/PagedRecords.scala
@@ -3,7 +3,7 @@ package models
 case class PagedRecords[+T] (
   currentPage: Int, // zero is the first page.
   pageSize: Int,    // the number of items in one page.
-  pageCount: Long,  // the number of pages.
+  pageCount: Long,  // the whle count of pages.
   orderBy: OrderBy,
   records: Seq[T]
 ) {
